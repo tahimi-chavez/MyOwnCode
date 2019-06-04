788f6c23c8e07c216103934e97c2d09d241370dc
tahimi-chavez <tahimi.chavez@gmail.com>
2019-06-04 14:32:40 -0400

    Add files via upload

---
+++ FunWithExceptions/FunWithExceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodA();
+            Console.Read();
+        }
+        static void MethodA()
+        {
+            MethodB();
+            
+        }
+        static void MethodB()
+        {
+            MethodC();
+        }
+        static void MethodC()
+        {
+            int x = 7;
+            int z = 0;
+            try
+            {
+                int y = x / z;
+            }
+            finally
+            {
+                Console.WriteLine("Finally!");
+            }
+
+        }
+    }
+}
