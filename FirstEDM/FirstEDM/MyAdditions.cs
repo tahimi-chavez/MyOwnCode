c1549947685ec74800de6eb9fd0e1cc303afd4fc
tahimi-chavez <tahimi.chavez@gmail.com>
2019-06-04 14:31:24 -0400

    Add files via upload

---
+++ FirstEDM/FirstEDM/MyAdditions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEDM
+{
+    public partial class Employee
+    {
+        public void DoNothing()
+        {
+        }
+    }
+}
---
+++ FirstEDM/FirstEDM/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEDM
+{
+    class Program
+    {
+        static FourthCoffeeEntities context = new FourthCoffeeEntities();
+        static void Main(string[] args)
+        {
+            var emps = from e in context.Employees
+                       select new { e.LastName, e.FirstName };
+            foreach (var item in emps)
+            {
+                Console.WriteLine(item.LastName);
+            }
+            Console.Read();
+        }
+    }
+}
