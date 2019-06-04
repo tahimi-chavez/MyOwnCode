22f2e02e563bd03ff66d5f1f973929d4c412b0ea
tahimi-chavez <tahimi.chavez@gmail.com>
2019-06-04 14:10:13 -0400

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    enum Cars : byte { Ford=5, GM=7, Mazda, Porsche, Toyota, Jeep, myCar=Jeep };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars myCar = Cars.Jeep;
+            Cars myCar2 = Cars.myCar;
+            DayOfWeek
+        }
+    }
+}
