1260c39c9d9911767e2630b4ba90d306caa4af32
tahimi-chavez <tahimi.chavez@gmail.com>
2019-06-04 14:29:26 -0400

    Add files via upload

---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = -10; i < 25; i++)
+            {
+
+                bool test = Convert.ToBoolean(i);
+                Console.WriteLine("int {0} is {1} bool",i,test); 
+            }
+            Console.Read();
+        }
+    }
+}
